957b50a7c664a71c494526f3ee21acff117b22c5
Christian Cavuti <christian.cavuti@gmail.com>
2025-12-10 19:30:22 +0100

    completed day 9

--- 09/main.py
+++ 09/main.py
@@ -1,10 +1,4 @@
-from pprint import pprint
 from collections import defaultdict, deque
-from functools import lru_cache
-from os import system
-
-import math
-
 data = []
 
 with open("src/i.txt") as file:
@@ -51,13 +45,13 @@ def fill_inside(p):
 
         if xr[0] == curr[0]:
             all_borders.add(curr)
-            # for ixi in range(min(xr[1], curr[1]), max(xr[1], curr[1])):
-            # all_borders.add((curr[0], ixi))
+            for ixi in range(min(xr[1], curr[1]), max(xr[1], curr[1])):
+                all_borders.add((curr[0], ixi))
             queue.append(xr)
         if xl[0] == curr[0]:
             all_borders.add(curr)
-            # for ixi in range(min(xl[1], curr[1]), max(xl[1], curr[1])):
-            # all_borders.add((curr[0], ixi))
+            for ixi in range(min(xl[1], curr[1]), max(xl[1], curr[1])):
+                all_borders.add((curr[0], ixi))
             queue.append(xl)
 
         yr = by[(iy + 1) % len(by)]
@@ -66,69 +60,92 @@ def fill_inside(p):
             all_borders.add(curr)
             for iyi in range(min(yr[0], curr[0]), max(yr[0], curr[0])):
                 all_borders.add((iyi, curr[1]))
-                # pass
             queue.append(yr)
         if yl[1] == curr[1]:
             all_borders.add(curr)
             for iyi in range(min(yl[0], curr[0]), max(yl[0], curr[0])):
                 all_borders.add((iyi, curr[1]))
-                # pass
             queue.append(yl)
 
     return all_borders
 
 
+# Totally not working
 def part2():
-    all_borders = sorted(fill_inside(points))
-    print(all_borders)
-    # input()
-    # all_tiles = sorted(fill_inside(all_borders))
+    all_borders = set(sorted(fill_inside(points)))
 
     borders_idx = defaultdict(list)
     for b in all_borders:
         borders_idx[b[0]].append(b[1])
-    print("indexed")
 
     for k in borders_idx.keys():
         borders_idx[k].sort(reverse=True)
-    print("sorted")
 
-    print(borders_idx)
-
+    conteggio = 0
     quads = {}
+    invalids = set()
     for p1 in points:
         for p2 in points:
-            if p1 != p2 and id(p1, p2) not in quads:
-                # print(p1, p2)
-                # print((p1[0], p2[1]), (p2[0], p1[1]))
-                # input()
+            conteggio += 1
+            print(conteggio)
+            if p1 != p2 and id(p1, p2) not in quads and id(p2, p1) not in invalids:
                 valid = True
                 if not (p1[0], p2[1]) in all_borders:
                     count = 0
-                    for c in borders_idx[p1[0]]:
+                    for pos, c in enumerate(borders_idx[p1[0]]):
                         if c < p2[1]:
                             break
-                        count += 1
+                        if (
+                            borders_idx[p1[0]][len(borders_idx[p1[0]]) - pos - 2]
+                            != c + 1
+                            or borders_idx[p1[0]][len(borders_idx[p1[0]]) - pos]
+                            != c - 1
+                        ):
+                            count += 1
                     if count % 2 == 0:
                         valid = False
+                    else:
+                        all_borders.add((p1[0], p2[1]))
 
-                if not (p2[0], p1[1]) in all_borders:
+                if not (p2[0], p1[1]) in all_borders and valid:
                     count = 0
-                    for c in borders_idx[p2[0]]:
+                    for pos, c in enumerate(borders_idx[p2[0]]):
                         if c < p1[1]:
                             break
-                        count += 1
+                        if (
+                            borders_idx[p2[0]][len(borders_idx[p2[0]]) - pos - 2]
+                            != c + 1
+                            or borders_idx[p2[0]][len(borders_idx[p2[0]]) - pos]
+                            != c - 1
+                        ):
+                            count += 1
                     if count % 2 == 0:
                         valid = False
+                    else:
+                        all_borders.add((p2[0], p1[1]))
 
                 if valid:
                     quads[id(p1, p2)] = (abs(p1[0] - p2[0]) + 1) * (
                         abs(p1[1] - p2[1]) + 1
                     )
-                    print(p1, p2, quads[id(p1, p2)])
+                else:
+                    invalids.add(id(p1, p2))
+
     print(max(quads.values()))
+
+
+def part2_alt():
+    from shapely import Polygon, box
+
+    polygon = Polygon(points)
+    maxx = 0
+    for p1 in points:
+        for p2 in points:
+            if p1 != p2 and polygon.contains(box(p1[0], p1[1], p2[0], p2[1])):
+                maxx = max(maxx, (abs(p1[0] - p2[0]) + 1) * (abs(p1[1] - p2[1]) + 1))
+    print(maxx)
 
 
 if __name__ == "__main__":
     part1()
-    part2()
+    part2_alt()
